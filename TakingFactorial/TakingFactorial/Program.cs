0cda783541b6d5fe00382f0104abf4b0c0dcb931
Alprnn357 <37711793+Alprnn357@users.noreply.github.com>
2024-03-21 18:13:15 +0300

    Add new project
    Add project named "TakingFactorial" written in C#

---
+++ TakingFactorial/TakingFactorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakingFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+
+            Console.Write("Enter a number (Bir sayı giriniz): ");
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", input, fakt(input));
+            Console.ReadKey();
+
+        }
+
+        static int fakt(int number)
+        {
+            int result = 1;
+            if(number == 0 || number == 1)
+            {
+                return result;
+            }
+            else if(number < 0)
+            {
+                return result;
+            }
+            else
+            {
+                return result * number * fakt(number - 1);
+            }
+
+        }
+    }
+}
